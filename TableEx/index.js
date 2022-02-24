5069dd6f353ecdae791b77a8b5e7565fea93fad5
Swarna <sdswarna1001@gmail.com>
2022-02-25 00:27:40 +0530

    Update index.js

--- TableEx/index.js
+++ TableEx/index.js
@@ -6,6 +6,9 @@ exports.handler = function (event, context, callback) {
         console.log(record.eventID);
         console.log(record.eventName);
         console.log('DynamoDB Record: %j', record.dynamodb);
+        console.log("TESTING");
+        console.log("CHANGES");
+        console.log("LOTS OF CHANGES");
     });
     callback(null, "message");
 };
